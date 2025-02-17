8edecd759eef7893fb7849498f03fb38d2947106
ganchun1130 <72029219+ganchun1130@users.noreply.github.com>
2025-02-17 18:33:58 +0800

    Update 1_start_xinference_serve.sh

--- API/1_start_xinference_serve.sh
+++ API/1_start_xinference_serve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 设置环境变量
-export XINFERENCE_HOME=/mnt/general/ganchun/code/xinference/cache
+export XINFERENCE_HOME=～/ganchun/code/xinference/cache
 
 # 启动 xinference-local 服务
 xinference-local --host 0.0.0.0 --port 9997

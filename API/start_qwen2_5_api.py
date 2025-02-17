b4c12b220522ffbe0c25e5942075fe0566d98ea6
ganchun1130 <72029219+ganchun1130@users.noreply.github.com>
2025-02-17 18:34:53 +0800

    Update start_qwen2_5_api.py

--- API/start_qwen2_5_api.py
+++ API/start_qwen2_5_api.py
@@ -6,7 +6,7 @@ client = RESTfulClient("http://0.0.0.0:9997")
 model_uid = client.launch_model(
     model_engine="transformers",
     model_name="qwen2.5-instruct",
-    model_path="/mnt/general/share/model/Qwen/Qwen2.5-0.5B-Instruct",
+    model_path="～/Qwen/Qwen2.5-0.5B-Instruct",
 )
 print('Model uid: ' + model_uid)
 # 以上是启动qwen的API服务

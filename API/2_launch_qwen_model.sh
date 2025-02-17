6a8b5dc67ff917e2a252d6636870171aa50be45d
ganchun1130 <72029219+ganchun1130@users.noreply.github.com>
2025-02-17 18:34:27 +0800

    Update 2_launch_qwen_model.sh

--- API/2_launch_qwen_model.sh
+++ API/2_launch_qwen_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# 要么运行这个文件，要么就运行/mnt/general/ganchun/code/XiaoYa/API/start_qwen2_5_api.py
+# 要么运行这个文件，要么就运行～/XiaoYa/API/start_qwen2_5_api.py
 # 启动 xinference 服务
-xinference launch --model_path /mnt/general/share/model/Qwen/Qwen2.5-0.5B-Instruct \
+xinference launch --model_path ～/Qwen/Qwen2.5-0.5B-Instruct \
                   --model-engine Transformers \
                   -n qwen2.5-instruct
